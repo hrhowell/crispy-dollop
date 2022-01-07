39b50c9b96602e633a6b04d5cda6834a13644dba
mpmoran2 <kamokumoran@gmail.com>
2022-01-06 16:06:41 -0800

    User Update attempt 2

--- Backend/p2RecAppBackend/recApp/src/main/java/com/p2/recApp/users/UserController.java
+++ Backend/p2RecAppBackend/recApp/src/main/java/com/p2/recApp/users/UserController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -64,8 +66,9 @@ public class UserController {
 	        return userService.downloadUserProfileImage(userID);
 	    }
 	
-	 @PostMapping(
-			 //RequestMapping may be better?
-			 )
+	 @RequestMapping(method=RequestMethod.POST, value="{userID}")
+	 public void updateUser(@RequestBody User user, @PathVariable int userID) {
+		 userService.updateUser(userID, user);
+	 }
 	
 }
